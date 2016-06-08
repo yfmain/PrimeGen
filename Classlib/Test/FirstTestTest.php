3c736e38eee0e9c6e7f391c8448c738d627d93df
Fu Yu <fu.yu@gmail.com>
2016-06-08 22:59:16 +0100

    UDPATE: Environment setup

---
+++ Classlib/Test/FirstTestTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use DevSpace\Test\Core\TestCase;
+
+class FirstTestTest extends TestCase
+{
+    public function testTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # PrimeGen
 Prime Number Generator
+
+Install
+clone master
+Run composer update in project root to install all packages
+
+Run Test
+Run Phpunit in project root
