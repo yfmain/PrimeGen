8f4a6615f231ac00744fdca30dfb57daae2bbd5c
Fu Yu <fu.yu@gmail.com>
2016-06-11 18:27:00 +0100

    UPDATE: Primes Class #13 Refactoring
    rename primes method method and return array of n prime numbers.

--- Classlib/MathLib/Primes.php
+++ Classlib/MathLib/Primes.php
@@ -3,25 +3,24 @@ namespace DevSpace\MathLib;
 
 class Primes
 {
-    private $primes = [2];
-    public function getTheNthPrime($n)
+    public function primes($n)
     {
+        $primes = [2];
         $count = 1;
         $i = 3;
         while ($count < $n) {
             $bound = sqrt($i);
             $prime = 3;
-            foreach ($this->primes as $prime) {
+            foreach ($primes as $prime) {
                 if ($prime > $bound) break;
                 if ($i % $prime == 0) break;
             }
             if ($prime > $bound) {
-                $this->primes[] = $i;
+                $primes[] = $i;
                 $count++;
             }
             $i += 2;
         }
-        return $this->primes[$n - 1];
+        return $primes;
     }
-
 }
--- Classlib/Test/MathLib/PrimesTest.php
+++ Classlib/Test/MathLib/PrimesTest.php
@@ -21,23 +21,33 @@ class PrimesTest extends TestCase
      */
     public function testGetNthPrimesReturnsCorrectPrimeNumber($n, $expected)
     {
-        $this->assertEquals($expected, $this->subject->getTheNthPrime($n));
+        $this->assertEquals($expected, max($this->subject->primes($n)));
     }
 
     public function TheNthPrimeProvider()
     {
         return array(
-//            'the 1st prime is 2'      =>  array(1, 2),
-//            'the 2nd prime is 3'      =>  array(2, 3),
-//            'the 3rd prime is 5'      =>  array(3, 5),
-//            'the 4th prime is 7'      =>  array(4, 7),
-//            'the 5th prime is 11'     =>  array(5, 11),
-//            'the 6th prime is 13'     =>  array(6, 13),
-//            'the 10th prime is 29'    =>  array(10, 29),
-//            "the 100th prime is 541"            => array(100, 541),
-//            "the 1Kth  prime is 7,919"          => array(1e3, 7919),
+            'the 1st prime is 2'      =>  array(1, 2),
+            'the 2nd prime is 3'      =>  array(2, 3),
+            'the 3rd prime is 5'      =>  array(3, 5),
+            'the 4th prime is 7'      =>  array(4, 7),
+            'the 5th prime is 11'     =>  array(5, 11),
+            'the 6th prime is 13'     =>  array(6, 13),
+            'the 10th prime is 29'    =>  array(10, 29),
+            "the 100th prime is 541"            => array(100, 541),
+            "the 1Kth  prime is 7,919"          => array(1e3, 7919),
 //            "the 10Kth prime is 104,729"        => array(1e4, 104729),
-            "the 100Kth prime is 1,299,709"     => array(1e5, 1299709),
+//            "the 100Kth prime is 1,299,709"     => array(1e5, 1299709),
+//            "the 1Mth prime is 15,485,863"      => array(1e6, 15485863),
+//            "the 10Mth prime is 179,424,673"    => array(1e7, 179424673)
+        );
+    }
+    
+    public function testPrimesReturnsNPrimeNumbers()
+    {
+        $this->assertEquals(
+            array(2, 3, 5, 7, 11),
+            $this->subject->primes(5)
         );
     }
 }
