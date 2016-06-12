0bf61b2e54267cdf86162ded696ccedecfbc96b1
Fu Yu <fu.yu@gmail.com>
2016-06-12 15:56:03 +0100

    UPDATE: Main app Primes refactoring
    added command prompt message and it now take arguments
    
    This is not nice, with a limited time, it works for the moment.

--- Classlib/Primes.php
+++ Classlib/Primes.php
@@ -5,17 +5,51 @@ use DevSpace\Apps\PrimesTableConsole;
 use DevSpace\Resources\Messages;
 use DevSpace\Validators\NaturalNumber;
 use DevSpace\MathLib\NaivePrimes;
+use DevSpace\MathLib\IncrementalSieve;
 use DevSpace\MathLib\TimesTable;
 use DevSpace\Services\PrimesTable;
 use DevSpace\Services\TableConsoleOutput;
 
+$shortopts = "";
+$shortopts .= "s:"; // Size
+$shortopts .= "n"; // Naive Primes Algorithms
+$options = getopt($shortopts);
+$size = isset($options['s']) ? $options['s'] : null;
+$isNaive = isset($options['n']);
+
 $validator = new NaturalNumber();
+if (!$validator->validate($size)) {
+    echo "-------------------------------------------------------------------------------";
+    echo "\r\n";
+    echo "The Primes Number Generator...";
+    echo "\r\n";
+    echo "-------------------------------------------------------------------------------";
+    echo "\r\n";
+    echo "  Input Arguments:";
+    echo "\r\n";
+    echo "      Size of primes table, as the first argument is always required.";
+    echo "\r\n";
+    echo "      -n optional, indicating usage of the naive version of primes algorithm.";
+    echo "\r\n\r\n";
+    echo "  Example:";
+    echo "\r\n";
+    echo "      php Primes.php 20 -n";
+    echo "\r\n";
+    echo "-------------------------------------------------------------------------------";
+    echo "\r\n";
+    return;
+}
+
 $messages = new Messages();
-$primesGenerator = new NaivePrimes();
 $timesTableGenerator = new TimesTable();
+$primesGenerator = $isNaive ? new NaivePrimes() : new IncrementalSieve();
 $primesTableService = new PrimesTable($primesGenerator, $timesTableGenerator);
 $outputService = new TableConsoleOutput(new Console_Table(CONSOLE_TABLE_ALIGN_RIGHT));
 
 $Primes = new PrimesTableConsole($messages, $validator, $outputService, $primesTableService);
 
-echo $Primes->run(1);
+echo "\r\n\r\n";
+echo !$isNaive ? "Prime Times Table - Incremental Sieve of Eratosthenes:" : "Prime Times Table - Naive Version:";
+echo "\r\n\r\n";
+echo $Primes->run($size);
+echo "\r\n\r\n";
